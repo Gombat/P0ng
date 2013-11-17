7831bb869ab3a437448a73d9b1a5d386fbe25256
wenndemann <wenndemann@gmail.com>
2013-11-17 23:30:26 +0000

    modify CPU

--- src/org/moparscape/gamedev/Game.java
+++ src/org/moparscape/gamedev/Game.java
@@ -12,7 +12,9 @@ public class Game extends BasicGame
 	static int fpsLimit = 60;
 	static int scoretxtright = width - 120;
 	static int txth1posx = width / 2 - 75;
-	static int gamemode = 1; // If gamemode == 0 -> Singelplayer; gamemode == 1 -> Multiplayer
+	static int gamemode = 0; // If gamemode == 0 -> Singelplayer; gamemode == 1 -> Multiplayer
+	static float yPosOld = 0;
+	static int cpuMaxSpeed = 5;
 	
 	Circle ball;
 	Rectangle paddlePlayer;
@@ -23,6 +25,7 @@ public class Game extends BasicGame
 	int scorePlayer;
 	int scorePlayer2;
 	int scoreCPU;
+	float speedPlayer = 0.0f, speedPlayer2 = 0.0f;
 
 	public Game(String title) {
 	   super(title);
@@ -42,30 +45,43 @@ public class Game extends BasicGame
 	
 	public void update(GameContainer gc, int delta) 
 	{
+		
 		if (gc.getInput().isKeyDown(Input.KEY_W)) {
-		   if (paddlePlayer.getMinY() > 0)
-		      paddlePlayer.setY(paddlePlayer.getY() - 10.0f);
+		   if (paddlePlayer.getMinY() > 0) {
+			   speedPlayer -= 1;
+			   paddlePlayer.setY(paddlePlayer.getY() - 10.0f);
+		   }
+		   
 		} else if (gc.getInput().isKeyDown(Input.KEY_S)) {
-		   if (paddlePlayer.getMaxY() < height)
-		      paddlePlayer.setY(paddlePlayer.getY() + 10.0f);
-		}
+		   if (paddlePlayer.getMaxY() < height) {
+			   speedPlayer += 1;
+			   paddlePlayer.setY(paddlePlayer.getY() + 10.0f);
+		   }
+			   
+		} else speedPlayer = 0;
 		
 		if (gamemode == 1) {
 		
 			if (gc.getInput().isKeyDown(Input.KEY_UP)) {
-			   if (paddlePlayer2.getMinY() > 0)
-			      paddlePlayer2.setY(paddlePlayer2.getY() - 10.0f);
+			   if (paddlePlayer2.getMinY() > 0) {
+				   speedPlayer2 -= 1;
+				   paddlePlayer2.setY(paddlePlayer2.getY() - 10.0f);
+			   }
 			} else if (gc.getInput().isKeyDown(Input.KEY_DOWN)) {
-			   if (paddlePlayer2.getMaxY() < height)
-			      paddlePlayer2.setY(paddlePlayer2.getY() + 10.0f);
-			}
+			   if (paddlePlayer2.getMaxY() < height) {
+				   speedPlayer2 += 1;
+				   paddlePlayer2.setY(paddlePlayer2.getY() + 10.0f);
+			   }
+			} else speedPlayer2 = 0;
 			
 		}
 	
 		ball.setLocation(ball.getX() + ballVelocity.getX(), ball.getY() + ballVelocity.getY());
 		
 		if (ball.getMinX() <= 0) {
-		   ballVelocity.x = -ballVelocity.getX();
+		   ballVelocity.x = 3;
+		   ball.setLocation(width / 2, height / 2);
+		   ballVelocity.y /= Math.abs(ballVelocity.getY());
 		   
 		   if (gamemode == 1) {
 			   scorePlayer2++;
@@ -73,27 +89,41 @@ public class Game extends BasicGame
 			   
 		}
 		if (ball.getMaxX() >= width) {
-		   ballVelocity.x = -ballVelocity.getX();
+		   ballVelocity.x = -3;
+		   ball.setLocation(width / 2, height / 2);
+		   ballVelocity.y /= Math.abs(ballVelocity.getY());
 		   scorePlayer++;
 		}
 		
-		if (ball.getMinY() <= 0)
-		   ballVelocity.y = -ballVelocity.getY();
-		if (ball.getMaxY() >= height)
+		if (ball.getMinY() <= 0 || ball.getMaxY() >= height)
 		   ballVelocity.y = -ballVelocity.getY();
 		
-		if (ball.intersects(paddlePlayer) || ball.intersects(paddlePlayer2)) {
-		   ballVelocity.x = -ballVelocity.getX();
+		if (ball.intersects(paddlePlayer)) {
+		   ballVelocity.x = - (ballVelocity.getX() * 1.1f);
+		   ballVelocity.y += speedPlayer;
 		}
 		
-		if (gamemode == 0) {
-			float ypos = ball.getCenterY() - paddleCPU.getHeight() / 2;
-			paddleCPU.setY(ypos); }
+		if (gamemode == 1) {
+			if (ball.intersects(paddlePlayer2)) {
+				ballVelocity.x = - (ballVelocity.getX() * 1.1f);
+				ballVelocity.y += speedPlayer2;
+			}
+		}
+		else {
+			if (ball.intersects(paddleCPU)) {
+				ballVelocity.x = - (ballVelocity.getX() * 1.1f);
+			}
+			float yPos = ball.getCenterY() - paddleCPU.getHeight() / 2;
+			float deltaY = yPos - yPosOld;
+			if(deltaY > cpuMaxSpeed) yPos = yPosOld + cpuMaxSpeed;
+			else if(deltaY < -cpuMaxSpeed) yPos = yPosOld - cpuMaxSpeed;
+			paddleCPU.setY(yPos); 
+			yPosOld = yPos; }
 	}
 	
 	public void render(GameContainer gc, Graphics g) 
 	{ 
-		g.drawString("Player 1: "+scorePlayer, 10, 25);
+		g.drawString("Player 1: " + scorePlayer, 10, 25);
 		
 		if (gamemode == 1) {
 			g.drawString("Player 2: "+scorePlayer2, scoretxtright, 25);
@@ -119,5 +149,7 @@ public class Game extends BasicGame
 		app.setTargetFrameRate(fpsLimit);
 		app.start();
 	}
+	
+
 }
 
